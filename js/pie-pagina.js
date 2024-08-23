027243b1242c7e785f72faf192365334eb3cdf11
Sofia-Reyes1033 <sofiadanielacastro@gmail.com>
2024-08-22 23:30:17 -0500

    first commit

---
+++ js/pie-pagina.js
@@ -0,0 +1,8 @@
+function cargarEncabezado8(){
+    fetch("/templates/encabezado-principal,html")
+.then(response=> response.text())
+.then(data => {document.getElementById('caja-encabezado-principal').innerHTML = data})
+.catch(error => alert("Error cargando el encabezado" ,error));
+
+}
+cargarEncabezado8();
